9fcb0385c878cb1cee2e293a70d134645d86c9de
Fanner487 <eamontang1@hotmail.com>
2018-01-29 12:52:21 +0000

    api urls pasted into project urls file

--- fyp/fyp/urls.py
+++ fyp/fyp/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from api import urls as api_urls
+
 urlpatterns = [
+	url(r'^api/',, include(api_urls, namespace="api")),
     url(r'^admin/', admin.site.urls),
 ]
