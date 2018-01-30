729cace9ece3d9d9f8a9e2e276332c77a03d795c
Fanner487 <eamontang1@hotmail.com>
2018-01-30 16:24:05 +0000

    added Event unit tests

--- fyp/api/tests.py
+++ fyp/api/tests.py
@@ -169,7 +169,7 @@ class EventTestCase(TestCase):
 
         response = self.create_event(data)
         print(response.json())
-        print(response.status)
+        print(response.status_code)
 
         # self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         # self.assertEqual(response.json().get('organiser'), "user1")
