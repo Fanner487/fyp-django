881a635d3921293ee75692537cd18d21f362f09e
Fanner487 <eamontang1@hotmail.com>
2018-01-30 15:52:48 +0000

    added Event unit tests

--- fyp/api/tests.py
+++ fyp/api/tests.py
@@ -118,10 +118,14 @@ class EventTestCase(TestCase):
     def setUp(self):
         client = APIClient()
 
-        self.create_user("user1" , "test1@gmail.com", "orangemonkeyeagle1")
-        self.create_user("user2" , "test2@gmail.com", "orangemonkeyeagle1")
-        self.create_user("user3" , "test3@gmail.com", "orangemonkeyeagle1")
-        self.create_user("user4" , "test4@gmail.com", "orangemonkeyeagle1")
+        # self.create_user("user1" , "test1@gmail.com", "orangemonkeyeagle1")
+        # self.create_user("user2" , "test2@gmail.com", "orangemonkeyeagle1")
+        # self.create_user("user3" , "test3@gmail.com", "orangemonkeyeagle1")
+        # self.create_user("user4" , "test4@gmail.com", "orangemonkeyeagle1")
+        User.objects.create_user("user1" , "test1@gmail.com", "orangemonkeyeagle1")
+        User.objects.create_user("user2" , "test2@gmail.com", "orangemonkeyeagle1")
+        User.objects.create_user("user3" , "test3@gmail.com", "orangemonkeyeagle1")
+        User.objects.create_user("user4" , "test4@gmail.com", "orangemonkeyeagle1")
 
 
     def test_event_create_success(self):
