a223aaf36f30bfe913ac455039402acb4d21c315
Fanner487 <eamontang1@hotmail.com>
2018-01-30 19:48:14 +0000

    created attempt unit tests

--- fyp/api/tests/test_attempt.py
+++ fyp/api/tests/test_attempt.py
@@ -6,7 +6,7 @@ from rest_framework.test import APIClient
 from rest_framework.test import APIRequestFactory
 from django.contrib.auth.models import User
 from rest_framework import status
-from ../.models import Event
+from ...models import Event
 
 import datetime
 from time import sleep
