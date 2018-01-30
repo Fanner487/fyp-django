4a4d919b8a18882fedc0a7ccdd46f726d67c8520
Fanner487 <eamontang1@hotmail.com>
2018-01-30 20:04:43 +0000

    created attempt unit tests

--- fyp/api/tests/test_attempt.py
+++ fyp/api/tests/test_attempt.py
@@ -107,3 +107,33 @@ class AttemptTestCase(TestCase):
         response3 = self.create_test_attempt_now("user2", event_id)
 
         self.assertEquals(response3.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+    def test_attempt_after_verification(self):
+
+        event_response = self.create_test_event_now()
+        self.assertEquals(event_response.status_code, status.HTTP_201_CREATED)
+
+        event_id = event_response.json().get('id')
+
+        sleep(2)
+
+        response1 = self.create_test_attempt_now("user2", event_id)
+        print(response1.status_code)
+        print(response1.json())
+
+        sleep(1)
+
+        response2 = self.create_test_attempt_now("user2", event_id)
+        print(response1.status_code)
+        print(response1.json())
+
+        self.assertEquals(response1.status_code, status.HTTP_201_CREATED)
+        self.assertEquals(response2.status_code, status.HTTP_201_CREATED)
+
+        event = Event.objects.get(id=event_id)
+        self.assertTrue("user2" in event.attending)
+
+        response3 = self.create_test_attempt_now("user2", event_id)
+
+        self.assertEquals(response3.status_code, status.HTTP_400_BAD_REQUEST)
