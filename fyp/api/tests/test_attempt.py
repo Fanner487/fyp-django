ea63f4490675604c68a6d382a655f0a70e7c4123
Fanner487 <eamontang1@hotmail.com>
2018-01-30 18:34:41 +0000

    created attempt unit tests

---
+++ fyp/api/tests/test_attempt.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework.test import APIRequestFactory
+from django.contrib.auth.models import User
+from rest_framework import status
+
+import datetime
+from time import sleep
+
+# Create your tests here.
+# User login test
+class AttemptTestCase(TestCase):
+
+    def create_user(self, username, email, password):
+        user = User.objects.create_user(username, email, password)
+        user.save()
+
+    def create_event(self, data):
+        return self.client.post("/api/events/", data=data, format='json')
+
+    def create_attempt(self, data):
+        return self.client.post("/api/attempts", data=data. forma='json')
+
+    def setUp(self):
+        client = APIClient()
+
+        self.create_user("user1" , "test1@gmail.com", "orangemonkeyeagle1")
+        self.create_user("user2" , "test2@gmail.com", "orangemonkeyeagle1")
+        self.create_user("user3" , "test3@gmail.com", "orangemonkeyeagle1")
+        self.create_user("user4" , "test4@gmail.com", "orangemonkeyeagle1")
+
+        event_start_time = datetime.datetime.now() + datetime.timedelta(seconds=2)
+        event_sign_in_time = event_start_time
+        event_finish_time = datetime.datetime(
+        	year=event_start_time.year,
+        	month=event_start_time.month,
+        	day=event_start_time.day,
+        	hour=23,
+        	minute=59,
+        	second=59)
+
+        print("Now: " + datetime.datetime.now())
+        print("Start: " + event_start_time)
+        print("Sign in: " + event_sign_in_time)
+        print("Finish: " + event_finish_time)
+        print("\n\n")
+
+
+    def test_attempt_success(self):
+
+        data = {
+            'organiser' :"user1",
+            'event_name' : "test1",
+            'location' : "nowhere",
+            'start_time' : self.event_start_time,
+            'finish_time' : self.event_finish_time,
+            'sign_in_time' : self.event_sign_in_time,
+            'attendees' : ['user2', 'user3', 'user4']
+        }
+
+        response = create_event(data)
+
+        print(response.statud_code)
