bcd193b4736852f5c597ccad4c00ddbe92fb44c8
Fanner487 <eamontang1@hotmail.com>
2018-01-31 00:32:52 +0000

    created user unit tests

--- fyp/api/tests/test_event.py
+++ fyp/api/tests/test_event.py
@@ -296,4 +296,6 @@ class EventTestCase(TestCase):
         # user 4
         update_response = self.client.patch("/api/events/" + str(response.json().get('id')) + "/", data_update, content_type='application/json')
         self.assertEquals(update_response.status_code, status.HTTP_200_OK)
+        self.assertEquals(update_response.json().get('event_name'), "update")
         self.assertEquals(update_response.json().get('location'), "update")
+        self.assertEquals(update_response.json().get('attendees'), ['user2', 'user3'])
