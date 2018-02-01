d988633523a908cd026bcee63fe4bf165c791e10
Fanner487 <eamontang1@hotmail.com>
2018-02-01 13:53:59 +0000

    -m

--- fyp/api/tests/test_attempt_serializer.py
+++ fyp/api/tests/test_attempt_serializer.py
@@ -116,7 +116,7 @@ class AttemptSerializerTestCase(TestCase):
         print("NEW CREATED: " + str(new_created))
 
         self.assertTrue(serializer.is_valid())
-        print(serializer.validdated_data['created'])
+        print(serializer.validated_data['created'])
         self.assertNotEquals(new_created, serializer.data['created'])
 
 
