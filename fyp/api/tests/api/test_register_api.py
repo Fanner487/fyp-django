56bcd9f8411e62c40a183e591c460f839f160611
Fanner487 <eamontang1@hotmail.com>
2018-02-02 17:58:59 +0000

    Added registration serializer and tests

--- fyp/api/tests/api/test_register_api.py
+++ fyp/api/tests/api/test_register_api.py
@@ -73,7 +73,7 @@ class RegisterTest(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(len(response.data['username']), 1)
+        self.assertEqual(len(response.data['password']), 1)
 
 
 
