23fccd9f57e001c6dc73d4791897d637a946b73a
Fanner487 <eamontang1@hotmail.com>
2018-02-02 18:55:12 +0000

    Added registration serializer and tests

--- fyp/api/tests/api/test_event_api.py
+++ fyp/api/tests/api/test_event_api.py
@@ -342,6 +342,8 @@ class EventUpdateTest(APITestCase):
             'attendees': ['user2', 'user3']
         })
 
+        print("\n\n" + self.url + str(self.test_event.id) + "/" + "\n\n")
+
         response = self.client.patch(self.url + str(self.test_event.id) + "/", data=new_data, content_type='application/json')
         # print(response.json())
         self.assertEqual(response.status_code, status.HTTP_200_OK)
