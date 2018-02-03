8c06dfc5ae0868834573ebcdeea795539430bead
Fanner487 <eamontang1@hotmail.com>
2018-02-03 16:06:29 +0000

    Event model test

--- fyp/api/tests/models/test_event_model.py
+++ fyp/api/tests/models/test_event_model.py
@@ -63,15 +63,15 @@ class EventModelTestCase(TestCase):
             attendees=['user2', 'user3', 'user4']
         )
 
-        self.assertEqual(event.organiser, "user1")
-        self.assertEqual(event.event_name, "test")
-        self.assertEqual(event.location, "test")
-        self.assertEqual(event.start_time, '2050-01-29T12:00:00')
-        self.assertEqual(event.finish_time, '2050-01-29T12:30:00')
-        self.assertEqual(event.sign_in_time, '2050-01-29T12:00:00')
-        self.assertEqual(event.attendees, ['user2', 'user3', 'user4'])
+        self.assertNotEqual(event.organiser, "user1")
+        self.assertNotEqual(event.event_name, "test")
+        self.assertNotEqual(event.location, "test")
+        self.assertNotEqual(event.start_time, '2050-01-29T12:00:00')
+        self.assertNotEqual(event.finish_time, '2050-01-29T12:30:00')
+        self.assertNotEqual(event.sign_in_time, '2050-01-29T12:00:00')
+        self.assertNotEqual(event.attendees, ['user2', 'user3', 'user4'])
         self.assertIsNone(event.attending)
-        self.assertEqual(event.attendance_required, False)
+        self.assertNotEqual(event.attendance_required, True)
 
         event.location = "new_location"
         event.save()
