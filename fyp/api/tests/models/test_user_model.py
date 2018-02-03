57d1a21eeffca57112503d6fa0f7127afc1d78fb
Fanner487 <eamontang1@hotmail.com>
2018-02-03 15:17:05 +0000

    Added comments to files

--- fyp/api/tests/models/test_user_model.py
+++ fyp/api/tests/models/test_user_model.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 from django.contrib.auth.models import User
-import pprint
+from pprint import pprint
 
 
 class UserModelTestCase(TestCase):
