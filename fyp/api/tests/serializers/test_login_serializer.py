f2dc522f8427b85cf8e4033760058def62a5e32d
Fanner487 <eamontang1@hotmail.com>
2018-02-03 14:27:43 +0000

    Added comments to files

---
+++ fyp/api/tests/serializers/test_login_serializer.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import TestCase
+from api.models import Event
+from django.contrib.auth.models import User
+from api import serializers
+
+
+
