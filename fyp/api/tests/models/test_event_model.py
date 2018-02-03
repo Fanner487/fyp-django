b08f04604a92efde75d8486c09852fe1a5b93f59
Fanner487 <eamontang1@hotmail.com>
2018-02-03 16:07:21 +0000

    Event model test

--- fyp/api/tests/models/test_event_model.py
+++ fyp/api/tests/models/test_event_model.py
@@ -42,15 +42,15 @@ class EventModelTestCase(TestCase):
             attendees=['user2', 'user3', 'user4']
         )
 
-        self.assertEqual(event.organiser, "user1NOT")
-        self.assertEqual(event.event_name, "testNOT")
-        self.assertEqual(event.location, "testNOT")
-        self.assertEqual(event.start_time, '2060-01-29T12:00:00')
-        self.assertEqual(event.finish_time, '2060-01-29T12:30:00')
-        self.assertEqual(event.sign_in_time, '2060-01-29T12:00:00')
-        self.assertEqual(event.attendees, ['user2', 'user3'])
+        self.assertNotEqual(event.organiser, "user1NOT")
+        self.assertNotEqual(event.event_name, "testNOT")
+        self.assertNotEqual(event.location, "testNOT")
+        self.assertNotEqual(event.start_time, '2060-01-29T12:00:00')
+        self.assertNotEqual(event.finish_time, '2060-01-29T12:30:00')
+        self.assertNotEqual(event.sign_in_time, '2060-01-29T12:00:00')
+        self.assertNotEqual(event.attendees, ['user2', 'user3'])
         # self.assertIsNone(event.attending)
-        self.assertEqual(event.attendance_required, False)
+        self.assertNotEqual(event.attendance_required, True)
 
     def test_event_create_user_update(self):
         event = Event.objects.create(
@@ -63,15 +63,15 @@ class EventModelTestCase(TestCase):
             attendees=['user2', 'user3', 'user4']
         )
 
-        self.assertNotEqual(event.organiser, "user1")
-        self.assertNotEqual(event.event_name, "test")
-        self.assertNotEqual(event.location, "test")
-        self.assertNotEqual(event.start_time, '2050-01-29T12:00:00')
-        self.assertNotEqual(event.finish_time, '2050-01-29T12:30:00')
-        self.assertNotEqual(event.sign_in_time, '2050-01-29T12:00:00')
-        self.assertNotEqual(event.attendees, ['user2', 'user3', 'user4'])
+        self.assertEqual(event.organiser, "user1")
+        self.assertEqual(event.event_name, "test")
+        self.assertEqual(event.location, "test")
+        self.assertEqual(event.start_time, '2050-01-29T12:00:00')
+        self.assertEqual(event.finish_time, '2050-01-29T12:30:00')
+        self.assertEqual(event.sign_in_time, '2050-01-29T12:00:00')
+        self.assertEqual(event.attendees, ['user2', 'user3', 'user4'])
         self.assertIsNone(event.attending)
-        self.assertNotEqual(event.attendance_required, True)
+        self.assertEqual(event.attendance_required, False)
 
         event.location = "new_location"
         event.save()
