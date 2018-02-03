bf07cf953c85c4aed6097316d94843d96c9be475
Fanner487 <eamontang1@hotmail.com>
2018-02-03 11:41:54 +0000

    Added password confirm in register use case

--- fyp/api/serializers.py
+++ fyp/api/serializers.py
@@ -334,7 +334,7 @@ def add_to_attending(username, event_id):
     print("Event_id to append: " + str(event_id))
     print("Attending: " + str(event.attendees))
 
-    if event.attending is None or username not in event.attending:
+    if username not in event.attending:
 
         print("Appending user")
         event.attending.append(username.strip().lower())
