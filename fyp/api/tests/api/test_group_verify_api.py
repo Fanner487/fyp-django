a33a950903be96d9b17a0ccf3b8bedec1c7200e6
Fanner487 <eamontang1@hotmail.com>
2018-02-11 13:39:10 +0000

    Fixed serializer typo

--- fyp/api/tests/api/test_group_verify_api.py
+++ fyp/api/tests/api/test_group_verify_api.py
@@ -52,6 +52,26 @@ class LoginTest(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_verify_group_empty(self):
+        data = {
+            'usernames': []
+        }
+
+        response = self.client.post(self.url, data=data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(response.data['non_field_errors']), 1)
+
+    def test_verify_group_non_existing_user(self):
+        data = {
+            'usernames': ['testuser1', 'testuser2', 'NOTAUSER']
+        }
+
+        response = self.client.post(self.url, data=data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(response.data['non_field_errors']), 1)
+
     # def test_login_incorrect_password(self):
     #     data = {
     #         'username': 'testuser',
