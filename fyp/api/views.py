72a55aa02f62fda0cf931eee6da914273db155fe
Fanner487 <eamontang1@hotmail.com>
2018-02-21 21:28:36 +0000

    Added jwt test

--- fyp/api/views.py
+++ fyp/api/views.py
@@ -21,8 +21,8 @@ This is the API access points for users, events and attempts
 
 class EventViewSet(ModelViewSet):
 
-    authentication_classes = (())
-    permission_classes = (())
+    authentication_classes = ()
+    permission_classes = ()
     """
     ModelViewSet for Event.
     GET, POST, PATCH operations and handling are generated from the parent class
