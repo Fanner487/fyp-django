e3f56e183e35b87df90c40e82e8aac103c399db2
Fanner487 <eamontang1@hotmail.com>
2018-02-27 16:15:15 +0000

    Update test

--- fyp/api/views.py
+++ fyp/api/views.py
@@ -32,6 +32,25 @@ class EventViewSet(ModelViewSet):
     serializer_class = EventSerializer
     queryset = Event.objects.all()
 
+    def update(self, request, *args, **kwargs):
+        # partial = kwargs.pop('partial', False)
+        # instance = self.get_object()
+        # serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        # serializer.is_valid(raise_exception=True)
+        # self.perform_update(serializer)
+        #
+        # if getattr(instance, '_prefetched_objects_cache', None):
+        #     # If 'prefetch_related' has been applied to a queryset, we need to
+        #     # forcibly invalidate the prefetch cache on the instance.
+        #     instance._prefetched_objects_cache = {}
+        #
+        # return Response(serializer.data)
+
+        print("In update")
+        return super().update(request, *args, **kwargs)
+
+
+
 
 class AttemptViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, GenericViewSet):
     """
