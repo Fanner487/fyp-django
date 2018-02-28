0a5ae268046c1faccf2df7310c5264327a4c6258
Fanner487 <eamontang1@hotmail.com>
2018-02-28 18:52:49 +0000

    Added test for jwt_obtain

---
+++ fyp/api/tests/api/test_api_token_auth_api.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from api.models import Event
+from rest_framework import status
+import json
+
+"""
+Tests all API calls for CRUD operations of the Event model.
+All potential invalid and non-nullable fields are tested
+"""
+class ApiTokenAuthApi(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.url = "/api/api-token-auth/"
+
+        self.username = "test1"
+        self.email = "test@example.com"
+        self.password = "mypassword"
+        self.user = User.objects.create_user(self.username, self.email, self.password)
+
+        self.login_data = {
+            'username': self.username,
+            'password': self.password
+        }
+
+    def test_token_obtain_success(self):
+
+        token_response = self.client.post(self.url, data=self.login_data, format='json')
+        self.assertEqual(token_response.status_code, status.HTTP_200_OK)
+
+    def test_token_obtain_wrong_username(self):
+        self.login_data = {
+            'username': "NOT",
+            'password': self.password
+        }
+
+        token_response = self.client.post(self.url, data=self.login_data, format='json')
+        self.assertEqual(token_response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_token_obtain_wrong_password(self):
+
+        self.login_data = {
+            'username': self.username,
+            'password': "NOT"
+        }
+
+        token_response = self.client.post(self.url, data=self.login_data, format='json')
+        self.assertEqual(token_response.status_code, status.HTTP_401_UNAUTHORIZED)
