0a5412d8fe9c5980f3a4a15c88f209b7315f1daa
Fanner487 <eamontang1@hotmail.com>
2018-02-28 20:04:01 +0000

    Added test for manual sign in

--- fyp/api/tests/api/test_manual_user_sign_in.py
+++ fyp/api/tests/api/test_manual_user_sign_in.py
@@ -81,6 +81,8 @@ class ManualUserSignInTest(TestCase):
         self.assertNotEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIsNotNone(response.data)
+        print("wrong event")
+        print(response.data)
         self.assertIsNone(response.data.get('non_field_errors'))
 
     def test_manual_sign_in_wrong_user(self):
@@ -95,7 +97,8 @@ class ManualUserSignInTest(TestCase):
         self.assertNotEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIsNotNone(response.data)
-        # print()
+        print("wrong user")
+        print(response.data)
         self.assertIsNone(response.data.get('non_field_errors'))
 
     def test_manual_sign_in_already_signed_in(self):
@@ -114,57 +117,7 @@ class ManualUserSignInTest(TestCase):
         self.assertNotEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIsNotNone(response.data)
+        print("already signed in")
+        print(response.data)
         self.assertIsNone(response.data.get('non_field_errors'))
 
-    # def test_token_obtain_wrong_username(self):
-    #     self.login_data = {
-    #         'username': "NOT",
-    #         'password': self.password
-    #     }
-    #
-    #     token_response = self.client.post(self.url, data=self.login_data, format='json')
-    #
-    #     self.assertEqual(token_response.status_code, status.HTTP_400_BAD_REQUEST)
-    #     self.assertIsNotNone(token_response.data)
-    #     self.assertIsNone(token_response.json().get('token'))
-    #     self.assertIsNotNone(token_response.json().get('non_field_errors'))
-    #
-    # def test_token_obtain_wrong_password(self):
-    #
-    #     self.login_data = {
-    #         'username': self.username,
-    #         'password': "NOT"
-    #     }
-    #
-    #     token_response = self.client.post(self.url, data=self.login_data, format='json')
-    #
-    #     self.assertEqual(token_response.status_code, status.HTTP_400_BAD_REQUEST)
-    #     self.assertIsNotNone(token_response.data)
-    #     self.assertIsNone(token_response.json().get('token'))
-    #     self.assertIsNotNone(token_response.json().get('non_field_errors'))
-    #
-    # def test_token_obtain_blank_username(self):
-    #
-    #     self.login_data = {
-    #         'password': "NOT"
-    #     }
-    #
-    #     token_response = self.client.post(self.url, data=self.login_data, format='json')
-    #
-    #     self.assertEqual(token_response.status_code, status.HTTP_400_BAD_REQUEST)
-    #     self.assertIsNotNone(token_response.data)
-    #     self.assertIsNone(token_response.json().get('token'))
-    #     self.assertIsNotNone(token_response.json().get('username'))
-    #
-    # def test_token_obtain_blank_password(self):
-    #
-    #     self.login_data = {
-    #         'username': self.username
-    #     }
-    #
-    #     token_response = self.client.post(self.url, data=self.login_data, format='json')
-    #
-    #     self.assertEqual(token_response.status_code, status.HTTP_400_BAD_REQUEST)
-    #     self.assertIsNotNone(token_response.data)
-    #     self.assertIsNone(token_response.json().get('token'))
-    #     self.assertIsNotNone(token_response.json().get('password'))
