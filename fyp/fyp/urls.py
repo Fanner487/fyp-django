8350cdecc26571c6a4d4fb4f7596f1f406a5dbc2
Fanner487 <eamontang1@hotmail.com>
2018-03-29 16:10:51 +0100

    Changed admin site

--- fyp/fyp/urls.py
+++ fyp/fyp/urls.py
@@ -21,6 +21,6 @@ from api import urls as api_urls
 
 urlpatterns = [
     url(r'^api/', include(api_urls, namespace="api")),
-    # url(r'^admin/', admin.site.urls),
-    url(r'^admin/', admin_site.urls),
+    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin_site.urls),
 ]
